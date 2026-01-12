6a93e8e822b347bbb434490e1b2ddd6cb72b18ac
Epharia <emelybarth@yahoo.com>
2026-01-12 02:21:54 +0100

    Add basic SSL plugin and configure server settings in Vite

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,19 @@
 import { defineConfig } from "vite";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 export default defineConfig({
     base: "/vrgame/",
+    plugins: [basicSsl()],
+    server: {
+        host: true,
+        port: 5173,
+        strictPort: true,
+        https: true,
+        hmr: {
+            protocol: "wss",
+        },
+    },
+    build: {
+        chunkSizeWarningLimit: 1000,
+    },
 });
