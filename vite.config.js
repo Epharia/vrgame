21199f5d7f5210857ffe65444fd0a81dbbfa358d
Epharia <emelybarth@yahoo.com>
2026-01-12 02:27:47 +0100

    test3

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
 import { defineConfig } from "vite";
-// import basicSsl from "@vitejs/plugin-basic-ssl";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 export default defineConfig({
     base: "/vrgame/",
-    // plugins: [basicSsl()],
-    // server: {
-    //     host: true,
-    //     port: 5173,
-    //     strictPort: true,
-    //     https: true,
-    //     hmr: {
-    //         protocol: "wss",
-    //     },
-    // },
+    plugins: [basicSsl()],
+    server: {
+        host: true,
+        port: 5173,
+        strictPort: true,
+        https: true,
+        hmr: {
+            protocol: "wss",
+        },
+    },
     build: {
         chunkSizeWarningLimit: 1000,
     },
